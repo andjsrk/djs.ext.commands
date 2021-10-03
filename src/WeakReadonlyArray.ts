0d1645f838e05121bea74d7d095c909ae4e08bc2
andjsrk <43586090+andjsrk@users.noreply.github.com>
2021-10-03 16:52:15 +0900

    Change JavaScript's `private(#)` to TypeScript's `protected`

--- src/WeakReadonlyArray.ts
+++ src/WeakReadonlyArray.ts
@@ -1,19 +1,19 @@
 export default class WeakReadonlyArray <T = any> {
-	#array: Array<T>
+	protected readonly _array: Array<T>
 	constructor(length: number)
 	constructor(...items: Array<T>)
 	constructor(lengthOrStartOfItems: number | T, ...items: Array<T>) {
 		if (typeof lengthOrStartOfItems === 'number') {
-			this.#array = new Array(lengthOrStartOfItems)
+			this._array = new Array(lengthOrStartOfItems)
 		} else {
-			this.#array = [ lengthOrStartOfItems, ...items ]
+			this._array = [ lengthOrStartOfItems, ...items ]
 		}
-		for (let i = 0; i < this.#array.length; i++) {
+		for (let i = 0; i < this._array.length; i++) {
 			const iInIteration = i
-			if (iInIteration in this.#array) { // must consider case that the index is empty
+			if (iInIteration in this._array) { // must consider case that the index is empty
 				Object.defineProperty(this, iInIteration, {
 					get: () => {
-						return this.#array[iInIteration]
+						return this._array[iInIteration]
 					},
 					writable: false,
 				})
@@ -22,9 +22,9 @@ export default class WeakReadonlyArray <T = any> {
 	}
 	readonly [index: number]: T
 	public [Symbol.iterator]() {
-		return this.#array[Symbol.iterator]()
+		return this._array[Symbol.iterator]()
 	}
 	public get length() {
-		return this.#array.length
+		return this._array.length
 	}
 }
